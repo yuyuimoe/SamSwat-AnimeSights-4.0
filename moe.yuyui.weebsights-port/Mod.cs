fb5666c499e0d6fe98423bcb743538c19e55d3a2
Yui <yui@osufx.com>
2025-11-03 16:00:50 -0300

    feat(mod): load locales and show error on failure

--- moe.yuyui.weebsights-port/Mod.cs
+++ moe.yuyui.weebsights-port/Mod.cs
@@ -1,14 +1,19 @@
-﻿using SPTarkov.DI.Annotations;
+﻿using moe.yuyui.weebsights_port.Locales;
+using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Models.Utils;
 
 namespace moe.yuyui.weebsights_port;
 
 [Injectable(TypePriority = OnLoadOrder.PostSptModLoader)]
-public class Mod(ISptLogger<Mod> logger): IOnLoad
+public class Mod(ISptLogger<Mod> logger, EnglishLocale englishLocale): IOnLoad
 {
     public Task OnLoad()
     {
+        if (!englishLocale.LoadLocales())
+        {
+            logger.Error("[Weeb Iron Sights] Failed to load locales, you may see garbled text");
+        }
         logger.Success("[Weeb Iron Sights] Items loaded successfully");
         return Task.CompletedTask;
     }
