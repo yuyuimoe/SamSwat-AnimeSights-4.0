61f688a40a427b498d534ac673cc41627d20f7a9
Yui <yui@osufx.com>
2025-11-03 15:35:12 -0300

    chore(git): initial commit

---
+++ moe.yuyui.weebsights-port/Mod.cs
@@ -0,0 +1,15 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Utils;
+
+namespace moe.yuyui.weebsights_port;
+
+[Injectable(TypePriority = OnLoadOrder.PostSptModLoader)]
+public class Mod(ISptLogger<Mod> logger): IOnLoad
+{
+    public Task OnLoad()
+    {
+        logger.Success("[Weeb Iron Sights] Items loaded successfully");
+        return Task.CompletedTask;
+    }
+}
---
+++ moe.yuyui.weebsights-port/ModMetadata.cs
@@ -0,0 +1,20 @@
+using SPTarkov.Server.Core.Models.Spt.Mod;
+
+namespace moe.yuyui.weebsights_port;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string Name { get; init; } = "Weeb Iron Sights";
+    public override string Author { get; init; } = "yuyui.moe";
+    public override List<string>? Contributors { get; init; } = ["SamSWAT"];
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
+        
+        
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string? License { get; init; } = "MIT";
+    public override string ModGuid { get; init; } = "moe.yuyui.weebsights-port";
+}
